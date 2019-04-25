4bad696a915eae18eaa252e1016c5109167da1de
Chloé Servières <c.servieres@simplon-charleville.fr>
2019-04-25 12:26:10 +0200

    Ajout d'un header provisoire

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <link href="css/headfoot.css" type="text/css" rel="stylesheet"/>
 <header>
   <div class="navbar">
+    <div class="logo">Cine-<strong>met</strong></div>
     <div><a href="index.php" class="nav">Accueil</a></div>
     <div><a href="#presentation" class="nav">Qui sommes nous ?</a></div>
     <div><a href="#" class="nav">Nos films</a></div>
