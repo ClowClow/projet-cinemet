089297cbb96818c3ffbbd28142eed01ff9ac494a
Chloé Servières <c.servieres@simplon-charleville.fr>
2019-04-26 13:20:59 +0200

    essais header

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
 <link href="css/headfoot.css" type="text/css" rel="stylesheet"/>
 <footer>
-  
+  <div class="one">
+  </div>
+  <hr/ class="foot">
+  <div class="two">
+    <p>©2019 Simplon Charleville pour Cinéma Charleville Mézières - Metropolis </p>
+  </div>
 </footer>
