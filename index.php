fa7e2bf7b633395b99de501963e9fa4844195d36
Chloé Servières <c.servieres@simplon-charleville.fr>
2019-04-29 09:52:08 +0200

    modifications carrousel (vendredi) + début partie présentation

--- index.php
+++ index.php
@@ -18,16 +18,36 @@
   <main>
     <div class="slider">
     </div>
-    <div class="carousel">
+    <!-- code pour le carroussel -->
+    <div class="carrousel">
+      <div class="leftarrow"><</div>
+      <div class="frame"><img id="frame" src="images/bella.jpg" width="200em" height="400em"></div>
+      <div class="frame"><img id="frame" src="images/marvel.jpg" width="200em" height="400em"></div>
+      <div class="frame"><img id="frame" src="images/dumbo.jpg" width="200em" height="400em"></div>
+      <div class="frame"><img id="frame" src="images/avengers.jpg" width="200em" height="400em"></div>
+      <div class="frame"><img id="frame" src="images/corgi.jpg" width="200em" height="400em"></div>
+      <div class="frame"><img id="frame" src="images/shazam.jpg" width="200em" height="400em"></div>
+      <div class="frame"><img id="frame" src="images/dblanche.jpg" width="200em" height="400em"></div>
+      <div class="rightarrow">></div>
     </div>
+    <!-- code pour la parallax -->
     <div class="parallax-window" data-parallax="scroll" data-image-src="images/cine.jpg"
     alt="salle de cinéma en parallax">
-
+    <!-- code pour la présentation -->
     </div>
     <div class="presentation" id="presentation">
+      <div class="texte">
       <h1 class="prestitre">Cine-<span class="bold">met</span></h1>
+      </br>
+      <p class="prestexte">Le meilleur du cinéma s'invite dans votre salon !!</p>
     </div>
-
+    <div class="images">
+      <img src="images/metropolis.jpg" alt="cinéma métropolis"
+      width="300em" height="200em"/>
+      <img src="images/salon.jpg" alt="image représentant un salon"
+      width="300em" height="200em"/>
+    </div>
+  </div>
   </main>
   <footer>
     <?php include "footer.php"?>
