6a32e09ddf4962d53f2353e404a1fae0ce9d3dbe
ClowClow <c.servieres@simplon-charleville.fr>
2019-07-31 16:23:02 +0200

    nouvelle image pour l'accueil admin + ajout des boutons panneau admin

--- version2/admin/portail_film.php
+++ version2/admin/portail_film.php
@@ -17,9 +17,23 @@
   <?php include "nav_admin.php"; ?>
   <center>
   <h2 class="mt-5">Cine-MET Gestion des films</h2>
-  <a href="add_film.php">
-    <button class="btn btn-success">Ajouter un film</button>
-  </a>
+  <div class="d-flex flex-column justify-content-center">
+    <div class="d-flex flex-row mt-5 justify-content-center">
+      <a href="add_film.php">
+        <button class="btn btn-success">Ajouter un film</button>
+      </a>
+    </div>
+    <div class="d-flex flex-row mt-3 justify-content-center">
+      <a href="#">
+        <button class="btn btn-success">Modifier un film</button>
+      </a>
+    </div>
+    <div class="d-flex flex-row mt-3 justify-content-center">
+      <a href="#">
+        <button class="btn btn-success">Supprimer un film</button>
+      </a>
+    </div>
+  </div>
   </center>
 </body>
 
--- version2/admin/portail_utilisateur.php
+++ version2/admin/portail_utilisateur.php
@@ -17,10 +17,28 @@
   <?php include "nav_admin.php"; ?>
   <center>
   <h2 class="mt-5">Cine-MET Gestion des utilisateurs</h2>
-
-  <a href="add_role.php">
-    <button class="btn btn-success">Ajouter un nouveau rôle</button>
-  </a>
+  <div class="d-flex flex-column justify-content-center">
+    <div class="d-flex flex-row mt-5 justify-content-center">
+      <a href="#">
+        <button class="btn btn-success">Ajouter un utilisateur</button>
+      </a>
+    </div>
+    <div class="d-flex flex-row mt-3 justify-content-center">
+      <a href="#">
+        <button class="btn btn-success">Modifier un utilisateur</button>
+      </a>
+    </div>
+    <div class="d-flex flex-row mt-3 justify-content-center">
+      <a href="#">
+        <button class="btn btn-success">Supprimer un utilisateur</button>
+      </a>
+    </div>
+    <div class="d-flex flex-row mt-3 justify-content-center">
+      <a href="add_role.php">
+        <button class="btn btn-success">Ajouter un nouveau rôle</button>
+      </a>
+    </div>
+  </div>
 
   </center>
 </body>
